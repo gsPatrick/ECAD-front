8fc3f1d2017a5c90871ed152233a45d1e5130a2f
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2026-02-19 20:22:21 -0300

    fix: use relative API path to support cross-domain session cookies

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.sbacem.com.br/apidois/api/extractor',
+    baseURL: '/api/extractor',
     withCredentials: true, // Important for sending cookies
 });
 
